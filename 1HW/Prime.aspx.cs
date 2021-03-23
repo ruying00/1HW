ab6ef771492442e636ad20ddf1d445077f40f6cb
MIS803 <MIS803@30803_PC38>
2021-03-23 15:25:29 +0800

    作業1

---
+++ 1HW/Prime.aspx.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace _1HW
+{
+    public partial class Prime : System.Web.UI.Page
+    {        
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            int a = 12;
+            int sum = 0;
+            for (int i = 1; i <= a; i++)
+            {
+                if (a % i == 0)
+                {
+                    sum += 1;
+                }
+            }
+                if (sum == 2)
+                {
+                    Response.Write("Yes");
+                }
+                else
+                {
+                    Response.Write("No");
+                }
+         }
+     }
+ }
